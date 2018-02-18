222d0f185badadf234569670f11f61d7ac3643f6
TJ Draper <tj@buzzingpixel.com>
2018-02-17 20:54:03 -0600

    Route messages single entry to controller action

--- dev/controllers/MessagesController.php
+++ dev/controllers/MessagesController.php
@@ -2,13 +2,13 @@
 
 namespace dev\controllers;
 
-use Craft;
-use dev\services\PaginationService;
 use yii\web\Response;
 use craft\elements\User;
 use craft\elements\Entry;
 use yii\web\HttpException;
 use craft\elements\Category;
+use craft\elements\db\AssetQuery;
+use dev\services\PaginationService;
 
 /**
  * Class SermonsController
@@ -106,7 +106,7 @@ class MessagesController extends BaseController
             'base' => PaginationService::getUriPathSansPagination()
         ]);
 
-        // TODO: move minification and caching here
+        // TODO: move caching here
         $response = $this->renderTemplate('_audio/index', compact(
             'shouldCache',
             'backLink',
@@ -120,5 +120,38 @@ class MessagesController extends BaseController
         ));
 
         return $response;
+    }
+
+    /**
+     * Renders the message single entry page
+     * @param Entry $entry
+     * @return Response
+     * @throws \Exception
+     */
+    public function actionEntry(Entry $entry) : Response
+    {
+        /** @var AssetQuery $shareImageQuery */
+        $shareImageQuery = $entry->customShareImage;
+
+        $shareImage = null;
+
+        if ($shareImageAsset = $shareImageQuery->one()) {
+            $shareImage = $shareImageAsset->getUrl([
+                'width' => min(1000, $shareImageAsset->width),
+            ]);
+        }
+
+        // TODO: move caching here
+        return $this->renderTemplate('_audio/index', [
+            'shouldCache' => true,
+            'noIndex' => ! $entry->searchEngineIndexing,
+            'metaTitle' => $entry->seoTitle ?? $entry->title,
+            'metaDescription' => $entry->seoDescription,
+            'shareImage' => $shareImage,
+            'heroHeading' => $entry->title,
+            'entries' => [$entry],
+            'backLink' => '/media/messages',
+            'backLinkText' => 'back to all messages',
+        ]);
     }
 }
