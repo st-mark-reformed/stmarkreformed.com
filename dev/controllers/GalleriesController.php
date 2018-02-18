d09d298b8134f8ea51bbf3adeea1c05fd0998888
TJ Draper <tj@buzzingpixel.com>
2018-02-17 21:34:00 -0600

    Move gallery single entry to no controller methodology

--- dev/controllers/GalleriesController.php
+++ dev/controllers/GalleriesController.php
@@ -5,6 +5,7 @@ namespace dev\controllers;
 use yii\web\Response;
 use craft\elements\Entry;
 use yii\web\HttpException;
+use craft\elements\db\AssetQuery;
 use dev\services\PaginationService;
 
 /**
@@ -58,5 +59,53 @@ class GalleriesController extends BaseController
         ));
 
         return $response;
+    }
+
+    /**
+     * Renders the gallery single entry page
+     * @param Entry $entry
+     * @return Response
+     * @throws \Exception
+     */
+    public function actionEntry(Entry $entry) : Response
+    {
+        /** @var AssetQuery $shareImageQuery */
+        $shareImageQuery = $entry->customShareImage;
+
+        $shareImage = null;
+
+        if ($shareImageAsset = $shareImageQuery->one()) {
+            $shareImage = $shareImageAsset->getUrl([
+                'width' => min(1000, $shareImageAsset->width),
+            ]);
+        }
+
+        /** @var AssetQuery $photoAssetsQuery */
+        $photoAssetsQuery = $entry->gallery;
+
+        $photoAssets = $photoAssetsQuery->all();
+
+        $heroImageAsset = null;
+
+        if ($photoAssets) {
+            $firstPhotoAsset = reset($photoAssets);
+            $heroImageAsset = $firstPhotoAsset;
+
+            if (! $shareImage) {
+                $shareImage = $heroImageAsset;
+            }
+        }
+
+        // TODO: move caching here
+        return $this->renderTemplate('_galleries/entry', [
+            'shouldCache' => true,
+            'noIndex' => ! $entry->searchEngineIndexing,
+            'metaTitle' => $entry->seoTitle ?? $entry->title,
+            'metaDescription' => $entry->seoDescription,
+            'shareImage' => $shareImage,
+            'heroHeading' => $entry->title,
+            'photoAssets' => $photoAssets,
+            'heroImageAsset' => $heroImageAsset,
+        ]);
     }
 }
