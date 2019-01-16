cee1966744e7b9687d77b72bb15d5b6adb269421
TJ Draper <tj@buzzingpixel.com>
2019-01-15 23:46:32 -0600

    Update fab build stuff

--- scripts/devUtilityScript.sh
+++ scripts/devUtilityScript.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-COUNTER=1;
+# COUNTER=1;
 
 while true; do
     chmod -R 0777 /app/storage;
     chmod -R 0777 /var/lib/mysql;
-    echo ${COUNTER} > /app/dev/null/tmp;
+    # echo ${COUNTER} > /app/dev/null/tmp;
     rsync -av /app/vendor/ /vendor-volume --delete
-    COUNTER=$((COUNTER+1));
+    # COUNTER=$((COUNTER+1));
     sleep 2;
 done
