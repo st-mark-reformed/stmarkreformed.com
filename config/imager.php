3441d4cf28884fd9be3004d4741457561f518f18
TJ Draper <tj@buzzingpixel.com>
2019-02-08 21:58:41 -0600

    Install Imager

---
+++ config/imager.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'cacheDuration' => 3155695200,
+    'imagerSystemPath' => '@webroot/imagecache/',
+    'imagerUrl' => '/imagecache/',
+    'removeMetadata' => true,
+];
