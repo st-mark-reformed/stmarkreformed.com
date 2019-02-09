2dc550a619da565b9ae4815f3fa2a1e85017871f
TJ Draper <tj@buzzingpixel.com>
2019-02-09 12:40:59 -0600

    Finish out replacing all transforms with imager and adding to transform job

--- dev/jobs/RunAssetTransformJob.php
+++ dev/jobs/RunAssetTransformJob.php
@@ -31,43 +31,101 @@ class RunAssetTransformJob extends BaseJob
         $imager = Imager::$plugin->imager;
 
         $imager->transformImage($asset, [
-            'allowUpscale' => false,
-            'width' => 500
+            'height' => 67,
+            'width' => 100,
+            'position' => $asset->getFocalPoint(),
+        ]);
+
+        $imager->transformImage($asset, [
+            'height' => 134,
+            'width' => 200,
+            'position' => $asset->getFocalPoint(),
+        ]);
+
+        $imager->transformImage($asset, [
+            'width' => 120,
+        ]);
+
+        $imager->transformImage($asset, [
+            'width' => 240,
+        ]);
+
+        $imager->transformImage($asset, [
+            'width' => 300,
         ]);
 
         $imager->transformImage($asset, [
             'allowUpscale' => false,
-            'width' => 800
+            'width' => 400,
         ]);
 
         $imager->transformImage($asset, [
             'allowUpscale' => false,
-            'width' => 1000
+            'width' => 500,
+        ]);
+
+        $imager->transformImage($asset, [
+            'width' => 600,
         ]);
 
         $imager->transformImage($asset, [
             'allowUpscale' => false,
-            'width' => 1200
+            'width' => 700,
         ]);
 
         $imager->transformImage($asset, [
             'allowUpscale' => false,
-            'width' => 1600
+            'width' => 800,
         ]);
 
         $imager->transformImage($asset, [
             'allowUpscale' => false,
-            'width' => 1920
+            'width' => 1000,
         ]);
 
         $imager->transformImage($asset, [
             'allowUpscale' => false,
-            'width' => 2400
+            'width' => 1200,
         ]);
 
         $imager->transformImage($asset, [
             'allowUpscale' => false,
-            'width' => 3840
+            'width' => 1400,
+        ]);
+
+        $imager->transformImage($asset, [
+            'allowUpscale' => false,
+            'width' => 1600,
+        ]);
+
+        $imager->transformImage($asset, [
+            'allowUpscale' => false,
+            'width' => 1800,
+        ]);
+
+        $imager->transformImage($asset, [
+            'allowUpscale' => false,
+            'width' => 1920,
+        ]);
+
+        $imager->transformImage($asset, [
+            'allowUpscale' => false,
+            'width' => 2400,
+        ]);
+
+        $imager->transformImage($asset, [
+            'allowUpscale' => false,
+            'width' => 2800,
+        ]);
+
+        $imager->transformImage($asset, [
+            'allowUpscale' => false,
+            'width' => 3600,
+        ]);
+
+        $imager->transformImage($asset, [
+            'allowUpscale' => false,
+            'width' => 3840,
         ]);
     }
 }
