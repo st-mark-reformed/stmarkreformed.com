2f156e282160ca7c18b767a7df61f1cf1d558790
TJ Draper <tj@buzzingpixel.com>
2019-02-09 11:50:03 -0600

    Update docker sync methods to run image transforms

--- sync.bat
+++ sync.bat
@@ -8,3 +8,4 @@ if "%1" == "" (
 
 docker-compose up -d
 docker exec -it --user root db-stmarkreformed bash -c "chmod +x /app/scripts/syncToLocal.sh;./app/scripts/syncToLocal.sh %ENV%"
+docker exec -it --user root php-stmarkreformed bash -c "chmod +x /app/scripts/runImageTransforms.sh;./app/scripts/runImageTransforms.sh"
