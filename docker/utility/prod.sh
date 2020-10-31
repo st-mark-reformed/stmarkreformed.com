fc2c5c73ab2aabb59eba3908216dda3667514ec9
TJ Draper <tj@buzzingpixel.com>
2020-10-31 15:51:39 -0500

    Add files volume to docker production config

--- docker/utility/prod.sh
+++ docker/utility/prod.sh
@@ -10,6 +10,7 @@ chmod -R 0777 /log-volume;
 
 while true; do
     chmod -R 0777 /image-cache-volume;
+    chmod -R 0777 /files-volume;
     chmod -R 0777 /storage-volume;
     chmod -R 0777 /uploads-volume;
     sleep 120;
