987c776d65c572b2656321aced60fab3d2f03da2
TJ Draper <tj@buzzingpixel.com>
2020-10-31 16:42:37 -0500

    Add public-cache-volume to production docker config

--- docker/utility/prod.sh
+++ docker/utility/prod.sh
@@ -11,6 +11,7 @@ chmod -R 0777 /log-volume;
 while true; do
     chmod -R 0777 /image-cache-volume;
     chmod -R 0777 /files-volume;
+    chmod -R 0777 /public-cache-volume;
     chmod -R 0777 /storage-volume;
     chmod -R 0777 /uploads-volume;
     sleep 120;
