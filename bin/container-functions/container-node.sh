b1a39f00a1a77f5d3a2e40a2153df0d7781b3185
TJ Draper <tj@buzzingpixel.com>
2021-10-13 21:48:28 -0500

    Add github ci

--- bin/container-functions/container-node.sh
+++ bin/container-functions/container-node.sh
@@ -14,15 +14,19 @@ function container-node() {
     if [[ -z "${allArgsExceptFirst}" ]]; then
         printf "${Yellow}Remember to 'exit' when you're done.${Reset}\n";
         docker run ${interactiveArgs} \
+            --name stmark-node \
             -v ${PWD}:/app \
             -w /app \
             ${nodeDockerImage} bash;
     else
         docker run ${interactiveArgs} \
+            --name stmark-node \
             -v ${PWD}:/app \
             -w /app \
             ${nodeDockerImage} bash -c "${allArgsExceptFirst}";
     fi
+
+    docker rm stmark-node >/dev/null 2>&1;
 
     return 0;
 }
