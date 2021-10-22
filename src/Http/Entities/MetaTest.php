ed0f28c2fa39a40710f04608da81bc9d66e88c48
TJ Draper <tj@buzzingpixel.com>
2021-10-21 22:42:18 -0500

    Add MetaTest

---
+++ src/Http/Entities/MetaTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Entities;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @psalm-suppress PropertyNotSetInConstructor
+ * @psalm-suppress MixedArrayAccess
+ */
+class MetaTest extends TestCase
+{
+    public function testMeta(): void
+    {
+        $meta = new Meta(metaTitle: 'testMetaTitle');
+
+        self::assertSame(
+            'testMetaTitle',
+            $meta->metaTitle(),
+        );
+
+        self::assertSame(
+            'St. Mark Reformed Church',
+            $meta->siteName(),
+        );
+
+        self::assertSame(
+            [],
+            $meta->stylesheets(),
+        );
+
+        self::assertSame(
+            ['https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js'],
+            $meta->jsFiles(),
+        );
+    }
+}
