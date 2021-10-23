c611a7f1da6864e9850bdeb06dd28a1774eac1a9
TJ Draper <tj@buzzingpixel.com>
2021-10-23 15:39:36 -0500

    Add ResizeByHeight ResizeOperationFactory

---
+++ src/Images/Resize/ResizeByHeight/ResizeOperations/ResizeOperationFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Images\Resize\ResizeByHeight\ResizeOperations;
+
+use App\Images\ImageCacheFileSystem;
+use App\Images\Resize\ImageResizeFactory;
+use App\Shared\Files\RemoteSplFileInfo;
+use App\Shared\Files\TempFilesFileSystem;
+use SplFileInfo;
+
+class ResizeOperationFactory
+{
+    public function __construct(
+        private ImageResizeFactory $imageResizeFactory,
+        private TempFilesFileSystem $tempFilesFileSystem,
+        private ImageCacheFileSystem $imageCacheFileSystem,
+    ) {
+    }
+
+    public function make(SplFileInfo $sourceFileInfo): ResizeOperationContract
+    {
+        if ($sourceFileInfo instanceof RemoteSplFileInfo) {
+            return new ResizeRemoteFile(
+                sourceFileInfo: $sourceFileInfo,
+                imageResizeFactory: $this->imageResizeFactory,
+                tempFilesFileSystem: $this->tempFilesFileSystem,
+                imageCacheFileSystem: $this->imageCacheFileSystem,
+            );
+        }
+
+        return new ResizeLocalFile(
+            sourceFileInfo: $sourceFileInfo,
+            imageResizeFactory: $this->imageResizeFactory,
+            imageCacheFileSystem: $this->imageCacheFileSystem,
+        );
+    }
+}
---
+++ src/Images/Resize/ResizeByHeight/ResizeOperations/ResizeOperationFactoryTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Images\Resize\ResizeByHeight\ResizeOperations;
+
+use App\Images\ImageCacheFileSystem;
+use App\Images\Resize\ImageResizeFactory;
+use App\Shared\Files\RemoteSplFileInfo;
+use App\Shared\Files\TempFilesFileSystem;
+use PHPUnit\Framework\TestCase;
+use SplFileInfo;
+
+/**
+ * @psalm-suppress PropertyNotSetInConstructor
+ * @psalm-suppress MixedArrayAccess
+ */
+class ResizeOperationFactoryTest extends TestCase
+{
+    private ResizeOperationFactory $factory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->factory = new ResizeOperationFactory(
+            imageResizeFactory: $this->createMock(
+                ImageResizeFactory::class,
+            ),
+            tempFilesFileSystem: $this->createMock(
+                TempFilesFileSystem::class,
+            ),
+            imageCacheFileSystem: $this->createMock(
+                ImageCacheFileSystem::class,
+            ),
+        );
+    }
+
+    public function testWhenInfoIsRemote(): void
+    {
+        self::assertInstanceOf(
+            ResizeRemoteFile::class,
+            $this->factory->make(
+                sourceFileInfo: new RemoteSplFileInfo(
+                    filename: 'test-filename.png',
+                    size: 534,
+                    content: 'test-content',
+                ),
+            ),
+        );
+    }
+
+    public function testWhenInfoIsLocal(): void
+    {
+        self::assertInstanceOf(
+            ResizeLocalFile::class,
+            $this->factory->make(
+                sourceFileInfo: new SplFileInfo(
+                    filename: 'test-filename.png',
+                ),
+            ),
+        );
+    }
+}
