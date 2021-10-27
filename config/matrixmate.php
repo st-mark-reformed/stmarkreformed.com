ee5fd6407239ce047f4b2e37799dee28ee31448a
TJ Draper <tj@buzzingpixel.com>
2021-10-27 12:01:05 -0500

    Set up the Simple CTA page builder block type

---
+++ config/matrixmate.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'pageBuilder' => [
+            'groups' => [
+                [
+                    'label' => 'CTAs',
+                    'types' => [
+                        'simpleCta',
+                        'imageContentCta',
+                    ],
+                ],
+                [
+                    'label' => 'Features',
+                    'types' => [
+                        'latestGalleries',
+                        'featuredSermonSeries',
+                    ],
+                ],
+            ],
+            'types' => [
+                // 'imageContentCta' => [
+                //     'tabs' => [
+                //         [
+                //             'label' => 'Test 1',
+                //             'fields' => ['preHeadline', 'Headline'],
+                //         ],
+                //         [
+                //             'label' => 'Test 2',
+                //             'fields' => ['contentField'],
+                //         ],
+                //     ],
+                //     'hiddenFields' => ['backgroundColor'],
+                // ],
+                // 'news' => ['maxLimit' => 1],
+            ],
+        ],
+    ],
+];
