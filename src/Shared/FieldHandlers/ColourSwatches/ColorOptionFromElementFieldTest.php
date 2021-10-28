23a64012d37a0907684bfb75881f028aeeabcf81
TJ Draper <tj@buzzingpixel.com>
2021-10-27 21:03:15 -0500

    Add ColorOptionFromElementField

---
+++ src/Shared/FieldHandlers/ColourSwatches/ColorOptionFromElementFieldTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\FieldHandlers\ColourSwatches;
+
+use craft\base\Element;
+use craft\errors\InvalidFieldException;
+use percipioglobal\colourswatches\models\ColourSwatches;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+/**
+ * @psalm-suppress PropertyNotSetInConstructor
+ * @psalm-suppress MixedArrayAccess
+ */
+class ColorOptionFromElementFieldTest extends TestCase
+{
+    private ColorOptionFromElementField $handler;
+    /**
+     * @var Element&MockObject
+     * @phpstan-ignore-next-line
+     */
+    private mixed $element;
+
+    /** @var mixed[] */
+    private array $elementCalls = [];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->elementCalls = [];
+
+        $colorsStdClass                  = new stdClass();
+        $colorsStdClass->testOptionParam = 'testOptionString';
+
+        $colors = [$colorsStdClass];
+
+        $colorModel = $this->createMock(
+            ColourSwatches::class,
+        );
+
+        $colorModel->method('colors')->willReturn($colors);
+
+        $this->element = $this->createMock(Element::class);
+
+        $this->element->method('getFieldValue')->willReturnCallback(
+            function (string $fieldHandle) use (
+                $colorModel,
+            ): ColourSwatches {
+                $this->elementCalls[] = [
+                    'method' => 'getFieldValue',
+                    'fieldHandle' => $fieldHandle,
+                ];
+
+                return $colorModel;
+            }
+        );
+
+        $this->handler = new ColorOptionFromElementField();
+    }
+
+    /**
+     * @throws InvalidFieldException
+     */
+    public function testGetStringValue(): void
+    {
+        $result = $this->handler->getStringValue(
+            element: $this->element,
+            fieldName: 'someFieldName',
+            option: 'testOptionParam',
+        );
+
+        self::assertSame(
+            'testOptionString',
+            $result,
+        );
+
+        self::assertCount(1, $this->elementCalls);
+
+        self::assertSame(
+            'getFieldValue',
+            $this->elementCalls[0]['method'],
+        );
+
+        self::assertSame(
+            'someFieldName',
+            $this->elementCalls[0]['fieldHandle'],
+        );
+    }
+}
