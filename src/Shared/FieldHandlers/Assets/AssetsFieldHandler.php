1e34caed3c559dda0c6ebc415d174aba07aab8c6
TJ Draper <tj@buzzingpixel.com>
2021-10-27 21:25:19 -0500

    Add AssetsFieldHandler

---
+++ src/Shared/FieldHandlers/Assets/AssetsFieldHandler.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\FieldHandlers\Assets;
+
+use craft\base\Element;
+use craft\elements\Asset;
+use craft\elements\db\AssetQuery;
+use craft\errors\InvalidFieldException;
+
+use function assert;
+
+/**
+ * @psalm-suppress MixedReturnTypeCoercion
+ */
+class AssetsFieldHandler
+{
+    /**
+     * @return Asset[]
+     *
+     * @throws InvalidFieldException
+     *
+     * @phpstan-ignore-next-line
+     */
+    public function getAll(
+        Element $element,
+        string $field,
+    ): array {
+        $query = $element->getFieldValue($field);
+
+        assert($query instanceof AssetQuery);
+
+        return $query->all();
+    }
+
+    /**
+     * @throws InvalidFieldException
+     *
+     * @phpstan-ignore-next-line
+     */
+    public function getOneOrNull(
+        Element $element,
+        string $field,
+    ): ?Asset {
+        $query = $element->getFieldValue($field);
+
+        assert($query instanceof AssetQuery);
+
+        $asset = $query->one();
+
+        assert($asset instanceof Asset || $asset === null);
+
+        return $asset;
+    }
+
+    /**
+     * @throws InvalidFieldException
+     *
+     * @phpstan-ignore-next-line
+     */
+    public function getOne(
+        Element $element,
+        string $field,
+    ): Asset {
+        $asset = $this->getOneOrNull(
+            element: $element,
+            field: $field,
+        );
+
+        assert($asset instanceof Asset);
+
+        return $asset;
+    }
+}
---
+++ src/Shared/FieldHandlers/Assets/AssetsFieldHandlerTest.php
@@ -0,0 +1,210 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\FieldHandlers\Assets;
+
+use craft\base\Element;
+use craft\elements\Asset;
+use craft\elements\db\AssetQuery;
+use craft\errors\InvalidFieldException;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use yii\base\InvalidConfigException;
+
+use function assert;
+
+/**
+ * @psalm-suppress PropertyNotSetInConstructor
+ * @psalm-suppress MixedArrayAccess
+ */
+class AssetsFieldHandlerTest extends TestCase
+{
+    private AssetsFieldHandler $handler;
+    /**
+     * @var Element&MockObject
+     * @phpstan-ignore-next-line
+     */
+    private mixed $element;
+
+    /** @var mixed[] */
+    private array $elementCalls = [];
+
+    private bool $queryOneReturnsElement = true;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->elementCalls = [];
+
+        $this->queryOneReturnsElement = true;
+
+        $asset1 = $this->createMock(Asset::class);
+        $asset1->method('getUrl')->willReturn('testUrl1');
+
+        $asset2 = $this->createMock(Asset::class);
+        $asset2->method('getUrl')->willReturn('testUrl2');
+
+        $this->element = $this->createMock(Element::class);
+
+        $query = $this->createMock(AssetQuery::class);
+
+        $query->method('one')->willReturnCallback(
+            function () use ($asset1): ?Asset {
+                if ($this->queryOneReturnsElement) {
+                    return $asset1;
+                }
+
+                return null;
+            }
+        );
+
+        $query->method('all')->willReturn([
+            $asset1,
+            $asset2,
+        ]);
+
+        $this->element->method('getFieldValue')->willReturnCallback(
+            function (string $fieldHandle) use (
+                $query,
+            ): AssetQuery {
+                $this->elementCalls[] = [
+                    'method' => 'getFieldValue',
+                    'fieldHandle' => $fieldHandle,
+                ];
+
+                return $query;
+            }
+        );
+
+        $this->handler = new AssetsFieldHandler();
+    }
+
+    /**
+     * @throws InvalidFieldException
+     * @throws InvalidConfigException
+     */
+    public function testGetAll(): void
+    {
+        $result = $this->handler->getAll(
+            element: $this->element,
+            field: 'testField',
+        );
+
+        self::assertCount(2, $result);
+
+        self::assertSame(
+            'testUrl1',
+            $result[0]->getUrl(),
+        );
+
+        self::assertSame(
+            'testUrl2',
+            $result[1]->getUrl(),
+        );
+
+        self::assertCount(1, $this->elementCalls);
+
+        self::assertSame(
+            'getFieldValue',
+            $this->elementCalls[0]['method'],
+        );
+
+        self::assertSame(
+            'testField',
+            $this->elementCalls[0]['fieldHandle'],
+        );
+    }
+
+    /**
+     * @throws InvalidFieldException
+     * @throws InvalidConfigException
+     */
+    public function testGetOneOrNullWhenHasOne(): void
+    {
+        $this->queryOneReturnsElement = true;
+
+        $result = $this->handler->getOneOrNull(
+            element: $this->element,
+            field: 'testField',
+        );
+
+        assert($result instanceof Asset);
+
+        self::assertSame(
+            'testUrl1',
+            $result->getUrl(),
+        );
+
+        self::assertCount(1, $this->elementCalls);
+
+        self::assertSame(
+            'getFieldValue',
+            $this->elementCalls[0]['method'],
+        );
+
+        self::assertSame(
+            'testField',
+            $this->elementCalls[0]['fieldHandle'],
+        );
+    }
+
+    /**
+     * @throws InvalidFieldException
+     */
+    public function testGetOneOrNullWhenDoesNotHaveOne(): void
+    {
+        $this->queryOneReturnsElement = false;
+
+        $result = $this->handler->getOneOrNull(
+            element: $this->element,
+            field: 'testField',
+        );
+
+        assert($result === null);
+
+        self::assertCount(1, $this->elementCalls);
+
+        self::assertSame(
+            'getFieldValue',
+            $this->elementCalls[0]['method'],
+        );
+
+        self::assertSame(
+            'testField',
+            $this->elementCalls[0]['fieldHandle'],
+        );
+    }
+
+    /**
+     * @throws InvalidFieldException
+     * @throws InvalidConfigException
+     */
+    public function testGetOne(): void
+    {
+        $this->queryOneReturnsElement = true;
+
+        $result = $this->handler->getOne(
+            element: $this->element,
+            field: 'testField',
+        );
+
+        self::assertSame(
+            'testUrl1',
+            $result->getUrl(),
+        );
+
+        self::assertCount(1, $this->elementCalls);
+
+        self::assertSame(
+            'getFieldValue',
+            $this->elementCalls[0]['method'],
+        );
+
+        self::assertSame(
+            'testField',
+            $this->elementCalls[0]['fieldHandle'],
+        );
+    }
+}
