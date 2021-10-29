6b6612a46f758f8b25152a00e5f2fe6e3f922177
TJ Draper <tj@buzzingpixel.com>
2021-10-29 13:00:14 -0500

    Fix an issue with static caching preventing craft live preview from working

--- config/di/configs/static-cache-middleware.php
+++ config/di/configs/static-cache-middleware.php
@@ -2,18 +2,46 @@
 
 declare(strict_types=1);
 
+use BuzzingPixel\SlimBridge\ServerRequestFactory;
 use BuzzingPixel\StaticCache\CacheApi\CacheApiContract;
 use BuzzingPixel\StaticCache\CacheApi\RedisCache\RedisCacheApi;
 use BuzzingPixel\StaticCache\StaticCacheMiddleware;
+use Psr\Container\ContainerInterface;
 
 use function DI\autowire;
 
 return [
     CacheApiContract::class => autowire(RedisCacheApi::class),
-    StaticCacheMiddleware::class => autowire(
-        StaticCacheMiddleware::class
-    )->constructorParameter(
-        'enabled',
-        (bool) getenv('STATIC_CACHE_ENABLED'),
-    ),
+    StaticCacheMiddleware::class => static function (
+        ContainerInterface $container,
+    ): StaticCacheMiddleware {
+        $enabled = (bool) getenv('STATIC_CACHE_ENABLED');
+
+        if ($enabled) {
+            $requestFactory = $container->get(ServerRequestFactory::class);
+
+            assert($requestFactory instanceof ServerRequestFactory);
+
+            $request = $requestFactory->make();
+
+            $params = $request->getQueryParams();
+
+            $liveKey = (string) ($params['x-craft-live-preview'] ?? '');
+
+            $previewKey = (string) ($params['x-craft-preview'] ?? '');
+
+            $live = $liveKey !== '' || $previewKey !== '';
+
+            $enabled = ! $live;
+        }
+
+        $cacheApi = $container->get(CacheApiContract::class);
+
+        assert($cacheApi instanceof RedisCacheApi);
+
+        return new StaticCacheMiddleware(
+            $enabled,
+            $cacheApi,
+        );
+    },
 ];
