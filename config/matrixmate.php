68672301000db186eb9d6a520e7ac89cc628f359
TJ Draper <tj@buzzingpixel.com>
2022-04-08 17:49:54 -0500

    Add upcoming events page build block type in the CP

--- config/matrixmate.php
+++ config/matrixmate.php
@@ -24,6 +24,7 @@ return [
                 [
                     'label' => 'Features',
                     'types' => [
+                        'latestEvents',
                         'latestGalleries',
                         'featuredSermonSeries',
                     ],
