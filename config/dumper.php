10c5eec529bc0bc466fbda88fe069daad8a69208
TJ Draper <tj@buzzingpixel.com>
2022-04-23 10:03:07 -0500

    Update symfony dumping configuration

--- config/dumper.php
+++ config/dumper.php
@@ -2,89 +2,102 @@
 
 declare(strict_types=1);
 
-// use Symfony\Component\VarDumper\Dumper\ContextProvider\CliContextProvider;
-// use Symfony\Component\VarDumper\Dumper\ContextProvider\SourceContextProvider;
+// phpcs:disable SlevomatCodingStandard.TypeHints.ReturnTypeHint.MissingAnyTypeHint
+// phpcs:disable SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingAnyTypeHint
+// phpcs:disable SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingTraversableTypeHintSpecification
 
+use Morrislaptop\VarDumperWithContext\HtmlDumper;
+use Symfony\Component\VarDumper\Cloner\Data;
 use Symfony\Component\VarDumper\Cloner\VarCloner;
 use Symfony\Component\VarDumper\Dumper\CliDumper;
-use Symfony\Component\VarDumper\Dumper\HtmlDumper;
+use Symfony\Component\VarDumper\Dumper\ContextProvider\CliContextProvider;
+use Symfony\Component\VarDumper\Dumper\ContextProvider\SourceContextProvider;
 use Symfony\Component\VarDumper\Dumper\ServerDumper;
 use Symfony\Component\VarDumper\VarDumper;
 
 $cloner = new VarCloner();
 
-$htmlDumper = new HtmlDumper();
+$htmlDumper = new class () extends HtmlDumper
+{
+    /** @phpstan-ignore-next-line */
+    protected function getCaller()
+    {
+        $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+
+        foreach ($backtrace as $trace) {
+            if (
+                /** @phpstan-ignore-next-line */
+                ! empty($trace['file']) &&
+                /** @phpstan-ignore-next-line */
+                ! empty($trace['line']) &&
+                ! str_contains($trace['file'], '/vendor/') &&
+                ! str_contains($trace['file'], 'dumper.php')
+            ) {
+                return $trace;
+            }
+        }
+
+        return [];
+    }
+
+    /** @phpstan-ignore-next-line */
+    public function dump(
+        Data $data,
+        $output = null,
+        array $extraDisplayOptions = []
+    ): void {
+        $checkForTwigDumperFile = debug_backtrace()[3]['file'] ?? '';
+
+        if (! $checkForTwigDumperFile) {
+            $checkForTwigDumperFile = debug_backtrace()[4]['file'] ?? '';
+        }
+
+        $checkForTwigDumperArray = explode(
+            DIRECTORY_SEPARATOR,
+            $checkForTwigDumperFile,
+        );
+
+        $isTwigDumper = $checkForTwigDumperArray[count($checkForTwigDumperArray) - 1] === 'TwigDumper.php';
+
+        if ($isTwigDumper) {
+            echo '<div style="background-color: #fff; display: inline-block; margin: 10px; padding: 25px;">';
+            parent::dump(
+                $data,
+                $output,
+                $extraDisplayOptions,
+            );
+            echo '</div><br>';
+
+            return;
+        }
+
+        parent::dump(
+            $data,
+            $output,
+            $extraDisplayOptions,
+        );
+    }
+};
 
 $htmlDumper->setTheme('light');
 
-$fallbackDumper = in_array(PHP_SAPI, ['cli', 'phpdbg'], true) ? new CliDumper() : $htmlDumper;
+$fallbackDumper = in_array(
+    PHP_SAPI,
+    ['cli', 'phpdbg'],
+    true,
+) ? new CliDumper() : $htmlDumper;
 
-// $dumper = new ServerDumper('tcp://127.0.0.1:9912', $fallbackDumper, [
-//     'cli' => new CliContextProvider(),
-//     'source' => new SourceContextProvider(),
-// ]);
+$contextProviders = [
+    'cli' => new CliContextProvider(),
+    'source' => new SourceContextProvider('UTF-8'),
+];
 
-$twigDumper = $dumper = new ServerDumper('tcp://127.0.0.1:9912', $fallbackDumper);
+$dumper = new ServerDumper(
+    'tcp://127.0.0.1:9912',
+    $fallbackDumper,
+    $contextProviders,
+);
 
-$varStore            = new stdClass();
-$varStore->hasDumped = false;
-
-VarDumper::setHandler(static function ($var) use ($cloner, $dumper, $twigDumper, $varStore): void {
-    /** @phpstan-ignore-next-line */
-    if (PHP_SAPI !== 'cli' && $varStore->hasDumped === false) {
-        echo '<head><title>Symfony Dumper</title></head><body>';
-        $varStore->hasDumped = true;
-    }
-
-    $checkForTwigDumperFile = debug_backtrace()[1]['file'] ?? '';
-
-    if (! $checkForTwigDumperFile) {
-        $checkForTwigDumperFile = debug_backtrace()[2]['file'] ?? '';
-    }
-
-    $checkForTwigDumperArray = explode(DIRECTORY_SEPARATOR, $checkForTwigDumperFile);
-
-    $isTwigDumper = $checkForTwigDumperArray[count($checkForTwigDumperArray) - 1] === 'TwigDumper.php';
-
-    if ($isTwigDumper) {
-        echo '<div></div>';
-        echo '<div style="background-color: #fff; display: inline-block; margin: 10px; padding: 25px;">';
-        echo '<pre style="font-size: 14px; margin-bottom: -10px; margin-left: 6px; background-color: #fff;">';
-        if (is_object($var)) {
-            echo $var::class;
-        } else {
-            echo gettype($var);
-        }
-
-        echo '</pre>';
-        $twigDumper->dump($cloner->cloneVar($var));
-        echo '</div><br>';
-
-        return;
-    }
-
-    $traceItem = debug_backtrace()[2];
-
-    if (PHP_SAPI !== 'cli') {
-        echo '<pre style="margin-bottom: -16px; background-color: #fff">';
-    }
-
-    echo $traceItem['file'] . ':' . $traceItem['line'] . ': ';
-
-    if (PHP_SAPI !== 'cli') {
-        echo '</pre>';
-        echo '<pre style="font-size: 14px; margin-bottom: -16px; margin-left: 6px; background-color: #fff">';
-    }
-
-    if (is_object($var)) {
-        echo $var::class . ' ';
-    } else {
-        echo gettype($var) . ' ';
-    }
-
-    if (PHP_SAPI !== 'cli') {
-        echo '</pre>';
-    }
-
+VarDumper::setHandler(static function ($var) use ($cloner, $dumper): void {
     $dumper->dump($cloner->cloneVar($var));
 });
