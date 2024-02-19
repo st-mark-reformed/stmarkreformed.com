2eab94e2c14abf377b88f40df1b48981df266453
TJ Draper <tj@buzzingpixel.com>
2024-02-19 16:43:12 -0600

    Add publication entry display page

--- src/Http/Response/Publications/DisplayPublicationEntryAction.php
+++ src/Http/Response/Publications/DisplayPublicationEntryAction.php
@@ -4,12 +4,77 @@ declare(strict_types=1);
 
 namespace App\Http\Response\Publications;
 
+use App\Http\Components\Hero\HeroFactory;
+use App\Http\Components\Link\Link;
+use App\Http\Entities\Meta;
+use App\Http\Shared\RouteParamsHandler;
+use App\Shared\FieldHandlers\Generic\GenericHandler;
+use BuzzingPixel\SlimBridge\ElementSetRoute\RouteParams;
+use craft\errors\InvalidFieldException;
 use Psr\Http\Message\ResponseInterface;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use yii\base\InvalidConfigException;
 
 class DisplayPublicationEntryAction
 {
+    public function __construct(
+        private HeroFactory $heroFactory,
+        private RouteParams $routeParams,
+        private GenericHandler $fieldHandler,
+        private RouteParamsHandler $routeParamsHandler,
+        private DisplayPublicationEntryResponder $responder,
+    ) {
+    }
+
+    /**
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws InvalidFieldException
+     * @throws InvalidConfigException
+     */
     public function __invoke(): ResponseInterface
     {
-        dd('here');
+        $entry = $this->routeParamsHandler->getEntry(
+            routeParams: $this->routeParams
+        );
+
+        $meta = new Meta(metaTitle: (string) $entry->title);
+
+        $breadcrumbs = [
+            new Link(
+                isEmpty: false,
+                content: 'Home',
+                href: '/',
+            ),
+            new Link(
+                isEmpty: false,
+                content: 'Men of the Mark',
+                href: '/publications/men-of-the-mark',
+            ),
+            new Link(
+                isEmpty: false,
+                content: 'Viewing Publication',
+                href: '',
+            ),
+        ];
+
+        $hero = $this->heroFactory->createFromDefaults();
+
+        if ($hero->heroHeading() === '') {
+            $hero = $hero->withHeroHeading(value: (string) $entry->title);
+        }
+
+        return $this->responder->respond(
+            $meta,
+            $hero,
+            $breadcrumbs,
+            $this->fieldHandler->getTwigMarkup(
+                $entry,
+                'body',
+            ),
+        );
     }
 }
---
+++ src/Http/Response/Publications/DisplayPublicationEntryResponder.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Response\Publications;
+
+use App\Http\Components\Hero\Hero;
+use App\Http\Entities\Meta;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Twig\Environment as TwigEnvironment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use Twig\Markup;
+
+class DisplayPublicationEntryResponder
+{
+    public function __construct(
+        private TwigEnvironment $twig,
+        private ResponseFactoryInterface $responseFactory,
+    ) {
+    }
+
+    /**
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function respond(
+        Meta $meta,
+        Hero $hero,
+        array $breadcrumbs,
+        Markup $content,
+    ): ResponseInterface {
+        $response = $this->responseFactory->createResponse();
+
+        $response = $response->withHeader(
+            'EnableStaticCache',
+            'true'
+        );
+
+        $response->getBody()->write($this->twig->render(
+            '@app/Http/Response/Pages/RenderPage/Standard/Page.twig',
+            [
+                'meta' => $meta,
+                'hero' => $hero,
+                'breadcrumbs' => new Markup(
+                    $this->twig->render(
+                        'Http/_Infrastructure/Breadcrumbs.twig',
+                        ['breadcrumbs' => $breadcrumbs],
+                    ),
+                    'UTF-8',
+                ),
+                'content' => new Markup(
+                    $this->twig->render(
+                        '@app/Http/Response/Publications/Content.twig',
+                        [ 'content' => $content ],
+                    ),
+                    'UTF-8',
+                ),
+            ]
+        ));
+
+        return $response;
+    }
+}
