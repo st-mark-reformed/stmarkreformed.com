54402de42da37ea64818da42889d814c414006eb
TJ Draper <tj@buzzingpixel.com>
2024-02-24 21:24:46 -0600

    Remove old Docker configuration

--- docker/scripts/sync-from-prod-02-db.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SQL_FILE_NAME="stmark_prod.sql";
-
-# Clear local database in prep for new dump
-mysqldump -u"${DB_USER}" -p"${DB_PASSWORD}" --add-drop-table --no-data site | grep ^DROP | mysql --init-command="SET SESSION FOREIGN_KEY_CHECKS=0;" -u"${DB_USER}" -p"${DB_PASSWORD}" "${DB_DATABASE}" --force;
-
-# Import database dump into local database
-mysql -u"${DB_USER}" -p"${DB_PASSWORD}" "${DB_DATABASE}" < "/opt/project/docker/localStorage/${SQL_FILE_NAME}";
