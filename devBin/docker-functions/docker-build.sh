9d5b7c01adf101dd551a7ae131a894740084694a
TJ Draper <tj@buzzingpixel.com>
2025-05-09 21:54:02 -0500

    Add queue consumer docker image

--- devBin/docker-functions/docker-build.sh
+++ devBin/docker-functions/docker-build.sh
@@ -9,26 +9,6 @@ function docker-build() {
 
     WORK_DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)";
 
-    # Run the proxy build
-    printf "${Cyan}Building ghcr.io/st-mark-reformed/stmarkreformed.com-proxy${Reset}\n";
-    DOCKER_BUILDKIT=1 docker build \
-        --build-arg BUILDKIT_INLINE_CACHE=1 \
-        --cache-from ghcr.io/st-mark-reformed/stmarkreformed.com-proxy \
-        --file docker/proxy/Dockerfile \
-        --tag ghcr.io/st-mark-reformed/stmarkreformed.com-proxy \
-        ${WORK_DIR};
-    printf "${Green}Finished building ghcr.io/st-mark-reformed/stmarkreformed.com-proxy${Reset}\n\n";
-
-    # Run the web build
-    printf "${Cyan}Building ghcr.io/st-mark-reformed/stmarkreformed.com-web${Reset}\n";
-    DOCKER_BUILDKIT=1 docker build \
-        --build-arg BUILDKIT_INLINE_CACHE=1 \
-        --cache-from ghcr.io/st-mark-reformed/stmarkreformed.com-web \
-        --file docker/web/Dockerfile \
-        --tag ghcr.io/st-mark-reformed/stmarkreformed.com-web \
-        ${WORK_DIR};
-    printf "${Green}Finished building ghcr.io/st-mark-reformed/stmarkreformed.com-web${Reset}\n\n";
-
     # Run the api build
     printf "${Cyan}Building ghcr.io/st-mark-reformed/stmarkreformed.com-api${Reset}\n";
     DOCKER_BUILDKIT=1 docker build \
@@ -38,6 +18,16 @@ function docker-build() {
         --tag ghcr.io/st-mark-reformed/stmarkreformed.com-api \
         ${WORK_DIR};
     printf "${Green}Finished building ghcr.io/st-mark-reformed/stmarkreformed.com-api${Reset}\n\n";
+
+    # Run the api-queue-consumer build
+    printf "${Cyan}Building ghcr.io/st-mark-reformed/stmarkreformed.com-api-queue-consumer${Reset}\n";
+    DOCKER_BUILDKIT=1 docker build \
+        --build-arg BUILDKIT_INLINE_CACHE=1 \
+        --cache-from ghcr.io/st-mark-reformed/stmarkreformed.com-api-queue-consumer \
+        --file docker/api-queue-consumer/Dockerfile \
+        --tag ghcr.io/st-mark-reformed/stmarkreformed.com-api-queue-consumer \
+        ${WORK_DIR};
+    printf "${Green}Finished building ghcr.io/st-mark-reformed/stmarkreformed.com-api-queue-consumer${Reset}\n\n";
 
     # Run the app build
     printf "${Cyan}Building ghcr.io/st-mark-reformed/stmarkreformed.com-app${Reset}\n";
@@ -49,15 +39,6 @@ function docker-build() {
         ${WORK_DIR};
     printf "${Green}Finished building ghcr.io/st-mark-reformed/stmarkreformed.com-app${Reset}\n\n";
 
-    # Run the schedule-runner build
-    printf "${Cyan}Building ghcr.io/st-mark-reformed/stmarkreformed.com-app-schedule-runner${Reset}\n";
-    DOCKER_BUILDKIT=1 docker build \
-        --build-arg BUILDKIT_INLINE_CACHE=1 \
-        --file docker/schedule-runner/Dockerfile \
-        --tag ghcr.io/st-mark-reformed/stmarkreformed.com-app-schedule-runner \
-        ${WORK_DIR};
-    printf "${Green}Finished building ghcr.io/st-mark-reformed/stmarkreformed.com-app-schedule-runner${Reset}\n\n";
-
     # Run the db build
     printf "${Cyan}Building ghcr.io/st-mark-reformed/stmarkreformed.com-db${Reset}\n";
     DOCKER_BUILDKIT=1 docker build \
@@ -68,6 +49,25 @@ function docker-build() {
         ${WORK_DIR};
     printf "${Green}Finished building ghcr.io/st-mark-reformed/stmarkreformed.com-db${Reset}\n\n";
 
+    # Run the proxy build
+    printf "${Cyan}Building ghcr.io/st-mark-reformed/stmarkreformed.com-proxy${Reset}\n";
+    DOCKER_BUILDKIT=1 docker build \
+        --build-arg BUILDKIT_INLINE_CACHE=1 \
+        --cache-from ghcr.io/st-mark-reformed/stmarkreformed.com-proxy \
+        --file docker/proxy/Dockerfile \
+        --tag ghcr.io/st-mark-reformed/stmarkreformed.com-proxy \
+        ${WORK_DIR};
+    printf "${Green}Finished building ghcr.io/st-mark-reformed/stmarkreformed.com-proxy${Reset}\n\n";
+
+    # Run the schedule-runner build
+    printf "${Cyan}Building ghcr.io/st-mark-reformed/stmarkreformed.com-app-schedule-runner${Reset}\n";
+    DOCKER_BUILDKIT=1 docker build \
+        --build-arg BUILDKIT_INLINE_CACHE=1 \
+        --file docker/schedule-runner/Dockerfile \
+        --tag ghcr.io/st-mark-reformed/stmarkreformed.com-app-schedule-runner \
+        ${WORK_DIR};
+    printf "${Green}Finished building ghcr.io/st-mark-reformed/stmarkreformed.com-app-schedule-runner${Reset}\n\n";
+
     # Run the utility build
     printf "${Cyan}Building ghcr.io/st-mark-reformed/stmarkreformed.com-utility${Reset}\n";
     DOCKER_BUILDKIT=1 docker build \
@@ -77,6 +77,16 @@ function docker-build() {
         --tag ghcr.io/st-mark-reformed/stmarkreformed.com-utility \
         ${WORK_DIR};
     printf "${Green}Finished building ghcr.io/st-mark-reformed/stmarkreformed.com-utility${Reset}\n\n";
+
+    # Run the web build
+    printf "${Cyan}Building ghcr.io/st-mark-reformed/stmarkreformed.com-web${Reset}\n";
+    DOCKER_BUILDKIT=1 docker build \
+        --build-arg BUILDKIT_INLINE_CACHE=1 \
+        --cache-from ghcr.io/st-mark-reformed/stmarkreformed.com-web \
+        --file docker/web/Dockerfile \
+        --tag ghcr.io/st-mark-reformed/stmarkreformed.com-web \
+        ${WORK_DIR};
+    printf "${Green}Finished building ghcr.io/st-mark-reformed/stmarkreformed.com-web${Reset}\n\n";
 
     return 0;
 }
