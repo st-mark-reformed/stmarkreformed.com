e491e1fdc25f8c45a7502abfb231200b12731ad7
TJ Draper <tdraper@rxante.com>
2025-05-14 13:51:59 -0500

    Fix an issue with craft app provisioning script

--- docker/application/pre-up-provisioning.sh
+++ docker/application/pre-up-provisioning.sh
@@ -13,3 +13,8 @@ docker run -it --rm \
     -v "${APP_DIR}:/var/www" \
     -w /var/www \
     ghcr.io/st-mark-reformed/stmarkreformed.com-app sh -c "composer install";
+
+cd ${APP_DIR};
+yarn install;
+yarn css;
+yarn javascript;
