664b998e53f9a41dfa16a09f1cf2ee1cd19811ed
TJ Draper <tdraper@rxante.com>
2025-06-02 16:25:30 -0500

    Add download button to the index page if there's only one downloadable resource and no body text

--- web/app/resources-test/ResourceListing.tsx
+++ web/app/resources-test/ResourceListing.tsx
@@ -1,5 +1,8 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable react/no-danger */
 import React from 'react';
 import Link from 'next/link';
+import { ArrowDownTrayIcon } from '@heroicons/react/24/outline';
 import { ResourceItem } from './repository/ResourceItem';
 import typography from '../typography/typography';
 
@@ -24,11 +27,26 @@ export default function ResourceListing (
                     />
                 </h3>
             </div>
+            {(() => {
+                if (entry.resourceDownloads.length !== 1 || entry.body) {
+                    return null;
+                }
+
+                return (
+                    <Link
+                        href={`/uploads/general/resources/${entry.slug}/${entry.resourceDownloads[0].filename}`}
+                        className="mt-auto w-full rounded-md bg-crimson px-3.5 py-2.5 text-sm text-white shadow-sm hover:bg-crimson-dark text-center mb-2"
+                        download
+                    >
+                        Download &ldquo;<span className="font-semibold">{entry.resourceDownloads[0].filename}</span>&rdquo; <span className="-mt-1 align-middle inline-block w-4 h-4"><ArrowDownTrayIcon /></span>
+                    </Link>
+                );
+            })()}
             <Link
                 href={href}
-                className="mt-auto w-full rounded-md bg-crimson px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-crimson-dark text-center"
+                className="mt-auto w-full rounded-md bg-crimson px-3.5 py-2.5 text-sm text-white shadow-sm hover:bg-crimson-dark text-center"
             >
-                Go to resource &raquo;
+                Go to resource page &raquo;
             </Link>
         </article>
     );
