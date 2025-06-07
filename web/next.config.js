5f6d3815fc487e8821a4934dd0185c4c86af1f3b
TJ Draper <tj@buzzingpixel.com>
2025-06-06 21:29:26 -0500

    Add redirects to the next app

--- web/next.config.js
+++ web/next.config.js
@@ -20,4 +20,43 @@ module.exports = {
     },
     poweredByHeader: false,
     reactStrictMode: true,
+    async redirects () {
+        return [
+            {
+                source: '/talent-show',
+                destination: 'https://docs.google.com/spreadsheets/d/14vULuTl1ikyzzz4mtK6Hb9j3flG7X_hsCHKil-CAIMg/edit?usp=sharing',
+                permanent: true,
+            },
+            {
+                source: '/sms',
+                destination: '/media/messages/series/a-theological-interpretation-of-american-history',
+                permanent: true,
+            },
+            {
+                source: '/pyles',
+                destination: 'https://www.gofundme.com/f/help-for-the-pyle-family-house-fire-recovery',
+                permanent: true,
+            },
+            {
+                source: '/prayer-list',
+                destination: 'https://docs.google.com/document/d/1dGsAepCpPRJXqvwL0LrUpX8GfWbeiQwuUmN18SgV5MA/edit',
+                permanent: true,
+            },
+            {
+                source: '/pancakes',
+                destination: 'https://docs.google.com/document/d/1yTjgLJdZOl6JYwmMd_nEChJCTx442uKvpH1pXCRT2zs/edit?usp=sharing',
+                permanent: true,
+            },
+            {
+                source: '/media/galleries/twelfth-night-january-2020',
+                destination: '/media/galleries/twelfth-night-january-2021',
+                permanent: true,
+            },
+            {
+                source: '/blake-purcell',
+                destination: 'https://www.facebook.com/tjdraper/posts/10161630527683682',
+                permanent: true,
+            },
+        ];
+    },
 };
