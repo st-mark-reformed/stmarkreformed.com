8ba57a173404ef5a08ea6a5dacf19a2cbaf5c027
TJ Draper <tj@buzzingpixel.com>
2025-06-07 22:21:05 -0500

    Fix incorrect static setting on home page

--- web/app/home-test/page.tsx
+++ web/app/home-test/page.tsx
@@ -7,7 +7,7 @@ import LatestGalleries from '../PageComponents/LatestGalleries';
 import LatestSermon from '../PageComponents/LatestSermon';
 import LatestNews from '../PageComponents/LatestNews';
 
-export const dynamic = 'force-static';
+export const dynamic = 'force-dynamic';
 
 export default async function Page () {
     return (
