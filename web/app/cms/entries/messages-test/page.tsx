f16283a616f773f7991df37f4ab375b4241a7799
TJ Draper <tj@buzzingpixel.com>
2025-06-10 21:19:11 -0500

    Refactor the empty state button strategy

--- web/app/cms/entries/messages-test/page.tsx
+++ web/app/cms/entries/messages-test/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Metadata } from 'next';
+import { PlusIcon } from '@heroicons/react/16/solid';
 import CmsLayout from '../../layout/CmsLayout';
 import { createPageTitle } from '../../../createPageTitle';
 import PageHeader from '../../layout/PageHeader';
@@ -18,11 +19,27 @@ export default async function Page () {
     return (
         <CmsLayout>
             <div className="mb-4 ">
-                <PageHeader title="Messages" />
+                <PageHeader
+                    title="Messages"
+                    buttons={[
+                        {
+                            id: 'newEntry',
+                            type: 'primary',
+                            content: (
+                                <>
+                                    <PlusIcon className="h-5 w-5 mr-1" />
+                                    New Entry
+                                </>
+                            ),
+                            href: '#newEntry',
+                        },
+                    ]}
+                />
             </div>
             <EmptyState
                 itemNameSingular="Entry"
                 itemNamePlural="Entries"
+                buttonHref="#newEntry"
             />
         </CmsLayout>
     );
--- web/app/cms/layout/EmptyState.tsx
+++ web/app/cms/layout/EmptyState.tsx
@@ -1,18 +1,24 @@
 import React, { BaseSyntheticEvent } from 'react';
 import { PlusIcon } from '@heroicons/react/20/solid';
 import { FolderPlusIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 export default function EmptyState (
     {
+        buttonHref = '',
         onButtonClick,
         itemNameSingular = 'item',
         itemNamePlural = 'items',
     }: {
+        buttonHref?: string;
         onButtonClick?: (event: BaseSyntheticEvent) => void;
         itemNameSingular?: string;
         itemNamePlural?: string;
     },
 ) {
+    const buttonClasses = 'inline-flex items-center rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-700 cursor-pointer';
+    const iconClasses = '-ml-0.5 mr-1.5 h-5 w-5';
+
     return (
         <div className="text-center rounded-lg border-2 border-dashed border-gray-300 p-6">
             <FolderPlusIcon className="mx-auto h-12 w-12 text-gray-400" />
@@ -27,18 +33,44 @@ export default function EmptyState (
                 {itemNameSingular}
                 .
             </p>
-            <div className="mt-6">
-                <button
-                    type="button"
-                    className="inline-flex items-center rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-700 cursor-pointer"
-                    onClick={onButtonClick}
-                >
-                    <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
-                    New
-                    {' '}
-                    {itemNameSingular}
-                </button>
-            </div>
+            {(() => {
+                if (!onButtonClick && !buttonHref) {
+                    return null;
+                }
+
+                return (
+                    <div className="mt-6">
+                        {(() => {
+                            if (onButtonClick) {
+                                return (
+                                    <button
+                                        type="button"
+                                        className={buttonClasses}
+                                        onClick={onButtonClick}
+                                    >
+                                        <PlusIcon className={iconClasses} aria-hidden="true" />
+                                        New
+                                        {' '}
+                                        {itemNameSingular}
+                                    </button>
+                                );
+                            }
+
+                            return (
+                                <Link
+                                    className={buttonClasses}
+                                    href={buttonHref}
+                                >
+                                    <PlusIcon className={iconClasses} aria-hidden="true" />
+                                    New
+                                    {' '}
+                                    {itemNameSingular}
+                                </Link>
+                            );
+                        })()}
+                    </div>
+                );
+            })()}
         </div>
     );
 }
