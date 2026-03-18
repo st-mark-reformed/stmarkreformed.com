d3e54583e213cb6b01a8348c7f18754946f3846a
TJ Draper <tj@buzzingpixel.com>
2026-03-18 09:57:57 -0500

    Update auth pre-up provisioning

--- docker/auth/pre-up-provisioning.sh
+++ docker/auth/pre-up-provisioning.sh
@@ -5,7 +5,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)";
 DOCKER_DIR=$(dirname "${SCRIPT_DIR}");
 PROJ_DIR=$(dirname "${DOCKER_DIR}");
-AUTH_DIR="${PROJ_DIR}/api";
+AUTH_DIR="${PROJ_DIR}/auth";
 
 docker run -it --rm \
     --entrypoint "" \
@@ -13,3 +13,15 @@ docker run -it --rm \
     -v "${AUTH_DIR}:/var/www" \
     -w /var/www \
     ghcr.io/st-mark-reformed/stmarkreformed.com-auth sh -c "composer install";
+
+cd ${AUTH_DIR} && pnpm install && npx @tailwindcss/cli -i ./assets/style.css -o ./public/assets/style.css
+
+#docker run --rm \
+#    --entrypoint "" \
+#    --env NODE_ENV=development \
+#    --env COREPACK_ENABLE_DOWNLOAD_PROMPT=0 \
+#    --env CI=1 \
+#    --name auth_provision \
+#    --mount type=bind,source="${AUTH_DIR}",target=/app \
+#    -w /app \
+#    node:22 bash -c "npm install -g corepack@latest && corepack enable && pnpm install && npx @tailwindcss/cli -i ./assets/style.css -o ./public/assets/style.css";
