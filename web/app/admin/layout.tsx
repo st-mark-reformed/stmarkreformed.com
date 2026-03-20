ddd0648321d4f19a9a8d52b7d04f928f13d57f88
TJ Draper <tj@buzzingpixel.com>
2026-03-19 21:47:29 -0500

    Make admin layout dynamic

--- web/app/admin/layout.tsx
+++ web/app/admin/layout.tsx
@@ -7,6 +7,14 @@ export const metadata: Metadata = {
     title: createPageTitle('Admin'),
 };
 
+/**
+ * Reading env/secrets config requires loading dynamically at runtime rather
+ * than build time. This ensures that all server components render dynamically
+ * because this is our root layout.
+ * @see https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
+ */
+export const dynamic = 'force-dynamic';
+
 export default async function AdminLayout (
     { children }: { children: ReactNode },
 ) {
