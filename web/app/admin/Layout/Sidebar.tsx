6e2b65238193f231947b36252e8e69c9ff6164f9
TJ Draper <tj@buzzingpixel.com>
2026-03-19 21:42:06 -0500

    Fix a typescript error

--- web/app/admin/Layout/Sidebar.tsx
+++ web/app/admin/Layout/Sidebar.tsx
@@ -28,7 +28,7 @@ export default async function Sidebar (
 
     return (
         <>
-            <SidebarCSR navigation={navigation} activeNav={activeNav} />
+            <SidebarCSR navigation={navigation} />
             <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col dark:bg-gray-800">
                 <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 dark:border-white/10 dark:bg-black/10">
                     <div className="flex h-16 shrink-0 items-center">
