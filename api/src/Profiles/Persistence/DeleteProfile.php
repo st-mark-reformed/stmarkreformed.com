395d487cf0d827f0de60b72189579f15ac4e32fa
TJ Draper <tj@buzzingpixel.com>
2026-03-22 14:11:52 -0500

    Add delete profile repository method

---
+++ api/src/Profiles/Persistence/DeleteProfile.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Profiles\Persistence;
+
+use App\Persistence\ApiPdo;
+use App\Result;
+use Ramsey\Uuid\UuidInterface;
+
+readonly class DeleteProfile
+{
+    public function __construct(private ApiPdo $pdo)
+    {
+    }
+
+    public function delete(UuidInterface $id): Result
+    {
+        $statement = $this->pdo->prepare(
+            'DELETE FROM profiles WHERE id = :id',
+        );
+
+        return new Result(
+            success: $statement->execute(['id' => $id->toString()]),
+        );
+    }
+}
--- api/src/Profiles/ProfilesRepository.php
+++ api/src/Profiles/ProfilesRepository.php
@@ -5,17 +5,30 @@ declare(strict_types=1);
 namespace App\Profiles;
 
 use App\Profiles\Persistence\CreateProfile;
+use App\Profiles\Persistence\DeleteProfile;
 use App\Result;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
 
 readonly class ProfilesRepository
 {
     public function __construct(
         private CreateProfile $createProfile,
+        private DeleteProfile $deleteProfile,
     ) {
     }
 
     public function createProfile(NewProfile $newProfile): Result
     {
         return $this->createProfile->create(newProfile: $newProfile);
+    }
+
+    public function deleteProfile(string|UuidInterface $id): Result
+    {
+        if (! $id instanceof UuidInterface) {
+            $id = Uuid::fromString($id);
+        }
+
+        return $this->deleteProfile->delete(id: $id);
     }
 }
