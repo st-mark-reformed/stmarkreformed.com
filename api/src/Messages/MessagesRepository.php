51235a81e6d3966f48ab8af2908358ad881cba31
TJ Draper <tj@buzzingpixel.com>
2026-03-27 22:30:50 -0500

    Add messages repository method to delete a message

--- api/src/Messages/MessagesRepository.php
+++ api/src/Messages/MessagesRepository.php
@@ -5,17 +5,29 @@ declare(strict_types=1);
 namespace App\Messages;
 
 use App\Messages\Persistence\CreateMessage;
+use App\Messages\Persistence\DeleteMessage;
+use App\Persistence\CreateUuid;
 use App\Result\Result;
+use Ramsey\Uuid\UuidInterface;
 
 readonly class MessagesRepository
 {
     public function __construct(
+        private CreateUuid $createUuid,
         private CreateMessage $createMessage,
+        private DeleteMessage $deleteMessage,
     ) {
     }
 
     public function create(NewMessage $message): Result
     {
         return $this->createMessage->create(message: $message);
+    }
+
+    public function delete(string|UuidInterface $id): Result
+    {
+        return $this->deleteMessage->delete(
+            id: $this->createUuid->fromStringOrInterface(id: $id),
+        );
     }
 }
