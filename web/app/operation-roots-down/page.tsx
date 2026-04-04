bcc9dfc6d74c8d3a060984a93f7cb9ad2a2df0b9
TJ Draper <tj@buzzingpixel.com>
2026-04-04 10:39:45 -0500

    Add Abe's edits to the Operation Roots Down page

--- web/app/operation-roots-down/page.tsx
+++ web/app/operation-roots-down/page.tsx
@@ -33,14 +33,18 @@ export default function OperationRootsDown () {
                     height={720}
                 />
                 <div className="mt-3 text-lg text-gray-600 prose max-w-none">
-                    <p className="text-center text-bronze-lightened-2 pb-2 pt-6 text-xl font-semibold">
+                    <p className="text-bronze-lightened-2 pb-2 pt-6 text-xl font-semibold">
                         In 2006, a group of families in the Nashville area began meeting together with the intent of establishing a congregation within the then-quite-young CREC.
                     </p>
-                    <p>That congregation was established about a year later, and our God has been uncommonly gracious and kind to Saint Mark Reformed Church during the intervening years, pouring out upon us blessing upon blessing as we have sought to faithfully &rdquo;serve Christ and the world through liturgy, mission, and community.&ldquo; But throughout that time, while the Lord has faithfully provided a number of different locations for us to meet on Sundays, He has not seen fit yet to bless us with a place uniquely &rdquo;our own&ldquo; – a place where we could put down roots and establish a lasting presence of worship and fellowship which we pray might grow and flourish for generations in that same location.</p>
+                    <p>That congregation was established about a year later, and our God has been uncommonly gracious and kind to Saint Mark Reformed Church during the intervening years, pouring out upon us blessing upon blessing as we have sought to faithfully &ldquo;serve Christ and the world through liturgy, mission, and community.&rdquo; But throughout that time, while the Lord has faithfully provided a number of different locations for us to meet on Sundays, He has not seen fit yet to bless us with a place uniquely &ldquo;our own&rdquo; &mdash; a place where we might put down roots and establish a lasting presence of worship and fellowship which we pray will grow and flourish for generations in that same location.</p>
 
-                    <p>The leadership of SMRC established a Building Fund a number of years ago which the Lord has blessed with steady and substantial growth. However, its value is still a good deal below what might be regarded as a &rdquo;respectable offer&ldquo; for any among several opportunities for acquiring such a property which have been manifested. Moreover, as additional opportunities continue to come to light, we now have an added sense of urgency to secure a new location within the next few months as the property where we have met for much of the past 15 years has recently been sold and we do not expect to be abe to continue meeting there past October of this year, when our current lease expires.</p>
+                    <p>The leadership of SMRC established a Building Fund a number of years ago which the Lord has blessed with steady and substantial growth. However, its value is still a good deal below what might be regarded as a &rdquo;respectable offer&ldquo; for any among several opportunities for acquiring such a property which have been manifested. Moreover, as additional opportunities continue to come to light, we now have an added sense of urgency to secure a new location within the next few months as the property where we have met for much of the past 15 years has recently been sold and we do not expect to be able to continue meeting there past October of this year, when our current lease expires.</p>
 
-                    <p>With these opportunities and challenges in mind, the Session of SMRC is launching &ldquo;Operation Roots Down,&rdquo; a focused fundraising effort with the ambitious goal of raising $3 million within the next few months. As a friend of this congregation, we appeal to you in Christ’s Name that, whether your means be small or great, you prayerfully consider an offering which would further us on toward this goal. Additionally, if you know of others within your own circle of acquaintances with the means and the inclination to add to these efforts, we humbly ask you to spread the word to them and speak a good word to them on our behalf.</p>
+                    <p className="text-bronze-lightened-2 pb-2 pt-2 text-xl font-semibold">
+                        With these opportunities and challenges in mind, the Session of SMRC is launching &ldquo;Operation Roots Down,&rdquo; a focused fundraising effort with the ambitious goal of raising $3 million within the next few months.
+                    </p>
+
+                    <p>As a friend of this congregation, we appeal to you in Christ’s Name that, whether your means be small or great, you <strong>prayerfully consider an offering</strong> which would further us on toward this goal. Additionally, if you know of others within your own circle of acquaintances with the means and the inclination to add to these efforts, we humbly ask you to <strong>spread the word</strong> to them and speak a good word to them on our behalf.</p>
 
                     <p>In the future we may be able to take payments online (though the cost of processing online payments is something to consider), but for now here are the ways you can give:</p>
 
@@ -48,7 +52,7 @@ export default function OperationRootsDown () {
                         <li>Place a check in the offering plate with &ldquo;Building Fund&rdquo; in the memo line.</li>
                         <li>Give a check directly to a deacon, TJ Draper or Randy Sadler (again, with &ldquo;Building Fund&rdquo; in the memo line).</li>
                         <li>
-                            <p className="mb-4">Mail a check to TJ Draper, Deacon and Treasurer (the P.O. box gets checked roughly twice a month and you could mail the check there, but it might sit there a while). Make the check out to Saint Mark Reformed Church.</p>
+                            <p className="mb-4">Mail a check to TJ Draper, Deacon and Treasurer (the P.O. box gets checked roughly twice a month and you could mail the check there, but it might sit there a while). Make the check payable to Saint Mark Reformed Church.</p>
                             <p>
                                 TJ Draper<br />
                                 1530 Halifax Dr.<br />
@@ -56,7 +60,7 @@ export default function OperationRootsDown () {
                             </p>
                         </li>
                         <li>
-                            To wire transfer funds, donate stock, or any other ideas for donations, please contact TJ Draper by email (<a href="mailto:tjdraper@stmarkreformed.com">tjdraper@stmarkreformed.com</a>) or phone (<a href="tel:+1-615-351-6284">615-351-6284</a>), or talk to him in-person at church.
+                            To wire transfer funds, donate stock, or any other ideas for donations, please contact TJ Draper by email (<a href="mailto:tjdraper@stmarkreformed.com">tjdraper@stmarkreformed.com</a>) or phone (<a href="tel:+1-615-351-6284">615-351-6284</a>), or talk to him in-person on Sunday morning.
                         </li>
                     </ul>
 
