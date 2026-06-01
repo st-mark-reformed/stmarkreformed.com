bd18756da2a611800999241bce708a95d7d298f3
TJ Draper <tj@buzzingpixel.com>
2026-05-31 22:31:50 -0500

    Add internal messages migrations

---
+++ api/config/Data/Migrations/20260531000002_create_internal_messages_table.php
@@ -0,0 +1,76 @@
+<?php
+
+/** @noinspection PhpIllegalPsrClassPathInspection */
+
+declare(strict_types=1);
+
+use Phinx\Db\Adapter\AdapterInterface;
+use Phinx\Migration\AbstractMigration;
+
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+// phpcs:disable Squiz.Classes.ClassFileName.NoMatch
+
+class CreateInternalMessagesTable extends AbstractMigration
+{
+    public function change(): void
+    {
+        $this->table(
+            'internal_messages',
+            [
+                'id' => false,
+                'primary_key' => ['id'],
+            ],
+        )->addColumn(
+            'id',
+            AdapterInterface::PHINX_TYPE_UUID,
+        )->addColumn(
+            'enabled',
+            AdapterInterface::PHINX_TYPE_BOOLEAN,
+            ['null' => false, 'default' => true],
+        )->addColumn(
+            'date',
+            AdapterInterface::PHINX_TYPE_DATETIME,
+            ['default' => null],
+        )->addColumn(
+            'title',
+            AdapterInterface::PHINX_TYPE_STRING,
+            ['null' => false, 'default' => ''],
+        )->addColumn(
+            'slug',
+            AdapterInterface::PHINX_TYPE_STRING,
+            ['null' => false, 'default' => ''],
+        )->addColumn(
+            'audio_path',
+            AdapterInterface::PHINX_TYPE_STRING,
+            ['null' => false, 'default' => ''],
+        )->addColumn(
+            'audio_file_size',
+            AdapterInterface::PHINX_TYPE_INTEGER,
+            ['null' => false, 'default' => 0, 'signed' => false],
+        )->addColumn(
+            'speaker_id',
+            AdapterInterface::PHINX_TYPE_UUID,
+            ['null' => false, 'default' => ''],
+        )->addColumn(
+            'passage',
+            AdapterInterface::PHINX_TYPE_STRING,
+            ['null' => false, 'default' => ''],
+        )->addColumn(
+            'series_id',
+            AdapterInterface::PHINX_TYPE_UUID,
+            ['null' => false, 'default' => ''],
+        )->addColumn(
+            'description',
+            AdapterInterface::PHINX_TYPE_TEXT,
+            ['null' => false, 'default' => ''],
+        )
+            ->addIndex(['enabled'])
+            ->addIndex(['date'])
+            ->addIndex(['title'])
+            ->addIndex(['slug'])
+            ->addIndex(['speaker_id'])
+            ->addIndex(['passage'])
+            ->addIndex(['series_id'])
+            ->create();
+    }
+}
