53839101226db3b58b0fa3424ea109d99d2f99d0
TJ Draper <tj@buzzingpixel.com>
2026-06-03 09:09:14 -0500

    Add men of the mark table to API

---
+++ api/config/Data/Migrations/20260603000001_create_men_of_the_mark_table.php
@@ -0,0 +1,53 @@
+<?php
+
+/** @noinspection PhpIllegalPsrClassPathInspection */
+
+declare(strict_types=1);
+
+use Phinx\Db\Adapter\AdapterInterface;
+use Phinx\Migration\AbstractMigration;
+
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+// phpcs:disable Squiz.Classes.ClassFileName.NoMatch
+
+class CreateMenOfTheMarkTable extends AbstractMigration
+{
+    public function change(): void
+    {
+        $this->table(
+            'men_of_the_mark',
+            [
+                'id' => false,
+                'primary_key' => ['id'],
+            ],
+        )->addColumn(
+            'id',
+            AdapterInterface::PHINX_TYPE_UUID,
+        )->addColumn(
+            'enabled',
+            AdapterInterface::PHINX_TYPE_BOOLEAN,
+            ['null' => false, 'default' => true],
+        )->addColumn(
+            'date',
+            AdapterInterface::PHINX_TYPE_DATETIME,
+            ['default' => null],
+        )->addColumn(
+            'title',
+            AdapterInterface::PHINX_TYPE_STRING,
+            ['null' => false, 'default' => ''],
+        )->addColumn(
+            'slug',
+            AdapterInterface::PHINX_TYPE_STRING,
+            ['null' => false, 'default' => ''],
+        )->addColumn(
+            'body',
+            AdapterInterface::PHINX_TYPE_TEXT,
+            ['null' => false, 'default' => ''],
+        )
+            ->addIndex(['enabled'])
+            ->addIndex(['date'])
+            ->addIndex(['title'])
+            ->addIndex(['slug'])
+            ->create();
+    }
+}
