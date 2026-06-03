bbb2bc440c271e8221484f81278c19cb6a728967
TJ Draper <tj@buzzingpixel.com>
2026-06-03 15:32:00 -0500

    Add pastor's page table

---
+++ api/config/Data/Migrations/20260603000002_create_pastors_page_table.php
@@ -0,0 +1,61 @@
+<?php
+
+/** @noinspection PhpIllegalPsrClassPathInspection */
+
+declare(strict_types=1);
+
+use Phinx\Db\Adapter\AdapterInterface;
+use Phinx\Migration\AbstractMigration;
+
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+// phpcs:disable Squiz.Classes.ClassFileName.NoMatch
+
+class CreatePastorsPageTable extends AbstractMigration
+{
+    public function change(): void
+    {
+        $this->table(
+            'pastors_page',
+            [
+                'id' => false,
+                'primary_key' => ['id'],
+            ],
+        )->addColumn(
+            'id',
+            AdapterInterface::PHINX_TYPE_UUID,
+        )->addColumn(
+            'enabled',
+            AdapterInterface::PHINX_TYPE_BOOLEAN,
+            ['null' => false, 'default' => true],
+        )->addColumn(
+            'date',
+            AdapterInterface::PHINX_TYPE_DATETIME,
+            ['default' => null],
+        )->addColumn(
+            'title',
+            AdapterInterface::PHINX_TYPE_STRING,
+            ['null' => false, 'default' => ''],
+        )->addColumn(
+            'slug',
+            AdapterInterface::PHINX_TYPE_STRING,
+            ['null' => false, 'default' => ''],
+        )->addColumn(
+            'heading',
+            AdapterInterface::PHINX_TYPE_STRING,
+            ['null' => false, 'default' => ''],
+        )->addColumn(
+            'subheading',
+            AdapterInterface::PHINX_TYPE_STRING,
+            ['null' => false, 'default' => ''],
+        )->addColumn(
+            'body',
+            AdapterInterface::PHINX_TYPE_TEXT,
+            ['null' => false, 'default' => ''],
+        )
+            ->addIndex(['enabled'])
+            ->addIndex(['date'])
+            ->addIndex(['title'])
+            ->addIndex(['slug'])
+            ->create();
+    }
+}
