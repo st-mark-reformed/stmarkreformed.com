3de83337e30d952ba474949a53e94234abb0869f
TJ Draper <tj@buzzingpixel.com>
2026-06-04 19:26:00 -0500

    Add resources table

---
+++ api/config/Data/Migrations/20260604000001_create_resources_table.php
@@ -0,0 +1,57 @@
+<?php
+
+/** @noinspection PhpIllegalPsrClassPathInspection */
+
+declare(strict_types=1);
+
+use Phinx\Db\Adapter\AdapterInterface;
+use Phinx\Migration\AbstractMigration;
+
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+// phpcs:disable Squiz.Classes.ClassFileName.NoMatch
+
+class CreateResourcesTable extends AbstractMigration
+{
+    public function change(): void
+    {
+        $this->table(
+            'resources',
+            [
+                'id' => false,
+                'primary_key' => ['id'],
+            ],
+        )->addColumn(
+            'id',
+            AdapterInterface::PHINX_TYPE_UUID,
+        )->addColumn(
+            'enabled',
+            AdapterInterface::PHINX_TYPE_BOOLEAN,
+            ['null' => false, 'default' => true],
+        )->addColumn(
+            'date',
+            AdapterInterface::PHINX_TYPE_DATETIME,
+            ['default' => null],
+        )->addColumn(
+            'title',
+            AdapterInterface::PHINX_TYPE_STRING,
+            ['null' => false, 'default' => ''],
+        )->addColumn(
+            'slug',
+            AdapterInterface::PHINX_TYPE_STRING,
+            ['null' => false, 'default' => ''],
+        )->addColumn(
+            'body',
+            AdapterInterface::PHINX_TYPE_TEXT,
+            ['null' => false, 'default' => ''],
+        )->addColumn(
+            'downloads',
+            AdapterInterface::PHINX_TYPE_TEXT,
+            ['null' => false, 'default' => ''],
+        )
+            ->addIndex(['enabled'])
+            ->addIndex(['date'])
+            ->addIndex(['title'])
+            ->addIndex(['slug'])
+            ->create();
+    }
+}
